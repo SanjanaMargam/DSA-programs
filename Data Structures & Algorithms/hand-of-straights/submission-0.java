bcc9c4d092aea09c20e18e09827f1da7cb300de9
Sanjana Margam <148646208+SanjanaMargam@users.noreply.github.com>
2026-07-01 00:43:26 +0530

    Add: hand-of-straights - submission-0

---
+++ Data Structures & Algorithms/hand-of-straights/submission-0.java
@@ -0,0 +1,39 @@
+class Solution {
+    public boolean isNStraightHand(int[] a, int k) {
+
+
+        if(a.length%k!=0) return false;
+
+        TreeMap<Integer,Integer> m=new TreeMap<>();
+
+        for(int i=0;i<a.length;i++){
+
+            m.put(a[i],m.getOrDefault(a[i],0)+1);
+
+        }
+
+        Arrays.sort(a);
+
+       while(!m.isEmpty()){
+
+        int start=m.firstKey();
+
+        for(int i=start;i<=start+k-1;i++)
+        {
+            if(!m.containsKey(i)) return false;
+
+            m.put(i,m.getOrDefault(i,0)-1);
+
+            if(m.get(i)<=0) m.remove(i);
+        }
+
+
+       }
+       return true;
+      
+
+
+
+        
+    }
+}
