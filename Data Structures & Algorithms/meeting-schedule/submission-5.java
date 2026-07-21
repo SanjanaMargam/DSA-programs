e9c8dc89246697cf9f4a4631444800ce4309a48e
Sanjana Margam <148646208+SanjanaMargam@users.noreply.github.com>
2026-07-21 14:38:07 +0530

    Add: meeting-schedule - submission-5

---
+++ Data Structures & Algorithms/meeting-schedule/submission-5.java
@@ -0,0 +1,45 @@
+/**
+ * Definition of Interval:
+ * public class Interval {
+ *     public int start, end;
+ *     public Interval(int start, int end) {
+ *         this.start = start;
+ *         this.end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+    public boolean canAttendMeetings(List<Interval> a) {
+
+
+if(a.size() ==0) return true;
+
+
+
+Collections.sort(a, (i1, i2) -> Integer.compare(i1.start, i2.start));
+
+        Interval i=a.get(0);
+
+        int endy=i.end;
+
+        for(int j=1;j<a.size();j++){
+ 
+
+            if(endy>=a.get(j).end) return false;
+
+            if(endy<a.get(j).end && endy>a.get(j).start) return false;
+            
+
+           
+
+            endy=a.get(j).end;
+
+        }
+
+        return true;
+
+
+
+    }
+}
